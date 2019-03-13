9e6676819db07da6e18a956efba169dd37a85730
wangding <408542507@qq.com>
2019-03-13 16:11:05 +0800

    http-api-test

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+
+    mochacli:{
+      options:{
+                reporter:'spec',
+             bail:true      
+      },
+  all:['test/*.js']  
+  }     
+                                        
+  });
+  
+
+
+
+
+
+
+ // mochacli:{
+   // options:{
+   //   reporter:'spec',
+    //  bail:true
+   // },
+    //all:['test/*.js']
+//  },
+//  run:{
+ // apiServer:{
+   // options:{
+     // wait:false
+    // },
+   //  args:['node app.js']
+ //   }
+  //}
+  //});
+  grunt.loadNpmTasks('grunt-mocha-cli');
+  //grunt.loadNpmTasks('run');
+  grunt.registerTask('default',['mochacli']);
+  //grunt.registerTask('default',['run','mochacli','stop'])
+
+
+}
